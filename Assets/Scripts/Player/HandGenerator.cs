695b6105a4ac5beeb2c23acef8415a7f7eee6cb8
Oprea Alexandru <oalexprea@yahoo.com>
2017-10-26 12:39:42 +0300

    Folder structure & Container selection for player hand

--- Assets/Scripts/Player/HandGenerator.cs
+++ Assets/Scripts/Player/HandGenerator.cs
@@ -6,45 +6,81 @@ public class HandGenerator : MonoBehaviour
 {
 
 	#region Parameters
-	// Interval between particles spawns.
-	public float spawnInterval = 0.025f;
+	// Interval between particles releases.
+	public float relaseInvterval = 0.025f;
 
-	//The last spawn time.
-	private float spawnTimer = 0f; 
+	//The last relase time.
+	private float relaseTimer = 0f; 
 
 	// Initial Force of the particle at spawn.
-	public float particleForce;
+	public float relaseForce;
 
-	// State of the particle generated.
-	public State particlesState;
+	// currently selected container.
+	public int currentState = 0;
+
+	// State of the particles hold inside the containers.
+	public State[] containers = new State[3];
+
 	#endregion
 
 	#region Update
+
 	private void Update()
 	{
-		if(Input.GetMouseButton(0))
+		SelectContainer ();
+
+		if (Input.GetMouseButton (0)) 
 		{
-			if (spawnTimer >= spawnInterval)
-			{
-				// It is time to spawn a new particle.
-
-				// Create the new particle object.
-				GameObject newParticle = ParticlePool.instance.RequestParticle(particlesState);
-
-				// Update particle parameters.
-				newParticle.GetComponent<Rigidbody2D>().AddForce(transform.right * particleForce);
-				newParticle.GetComponent<Substance>().ChangeSubstanceState(particlesState);
-				newParticle.transform.position = transform.position;
-
-				// Reset timer.
-				spawnTimer = 0f; 	
-			}
-			else
-			{
-				spawnTimer += Time.deltaTime;
-			}	
+			Relase ();
 		}
+		else if(Input.GetMouseButton(1))
+		{
+			Collect ();
+		}
+	}
+
+	private void SelectContainer()
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			currentState = 0;
+		else if (Input.GetKeyDown (KeyCode.Alpha2))
+			currentState = 1;
+		else if (Input.GetKeyDown (KeyCode.Alpha3))
+			currentState = 2;
+	}
+
+	private void Relase()
+	{
+		if (relaseTimer >= relaseInvterval) 
+		{
+			// It is time to spawn a new particle.
+
+			// Get the current state of the selected container.
+			State particleState = containers [currentState];
+
+			// Create the new particle object.
+			GameObject newParticle = ParticlePool.instance.RequestParticle (particleState);
+
+			// Update particle parameters.
+			newParticle.GetComponent<Rigidbody2D> ().AddForce (transform.right * relaseForce);
+			newParticle.GetComponent<Substance> ().ChangeSubstanceState (particleState);
+			newParticle.transform.position = transform.position;
+
+			// Reset timer.
+			relaseTimer = 0f; 	
+		} 
+
+		else 
+		{
+			relaseTimer += Time.deltaTime;
+		}	
 
 	}
+
+	private void Collect()
+	{
+		
+	}
+
 	#endregion
 }

3e49ca97e1a6a452a3c2271b5422dcec615bcb9f
VladDuncea <vlad_duncea_31@yahoo.com>
2017-10-29 18:28:08 +0200

    SplashScreen script + Menu scene

---
+++ Assets/Scripts/Autoloader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Autoloader : MonoBehaviour
+{
+    #region Variabiles
+    public string sceneName;
+
+    public AudioClip introSound;
+    private AudioSource audioS;
+    #endregion
+
+    #region Loading Scene after sounds end
+    private void Start()
+    {
+        PlayIntroSound();
+        StartCoroutine(LoadSceneCR());
+    }
+
+    private void PlayIntroSound()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.volume = .1f;
+        audioS.PlayOneShot(introSound);
+    }
+
+    private IEnumerator LoadSceneCR()
+    {
+        yield return new WaitForSeconds(introSound.length + .5f);
+        SceneManager.LoadScene(sceneName);
+    }
+    #endregion
+}

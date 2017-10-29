9bb8d86ce25a3b06944a938ef59b14eb363e911e
VladDuncea <vlad_duncea_31@yahoo.com>
2017-10-29 18:49:29 +0200

    MenuScript

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    #region Variabiles
+    public AudioClip buttonSound;
+    private AudioSource audioS;
+    #endregion
+
+    #region Initialization
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.volume = .5f;
+    }
+    #endregion
+
+    #region Changing Scene
+    public void ChangeScene(string scene)
+    {
+        Time.timeScale = 1f;
+        audioS.PlayOneShot(buttonSound);
+        StartCoroutine(ChangeSceneCR(scene));
+    }
+
+    private IEnumerator ChangeSceneCR(string scene)
+    {
+        yield return new WaitForSeconds(buttonSound.length);
+        SceneManager.LoadScene(scene);
+        yield break;
+    }
+    #endregion
+
+    
+    #region Methods
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    #endregion
+    
+}

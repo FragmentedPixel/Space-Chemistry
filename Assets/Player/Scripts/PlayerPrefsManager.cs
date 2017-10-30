30df2ecc2f0b5cb8ee523018ff7c427a6423e4a8
VladDuncea <vlad_duncea_31@yahoo.com>
2017-10-30 21:41:03 +0200

    Persistent Data

---
+++ Assets/Player/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+public class PlayerPrefsManager : MonoBehaviour
+{
+    #region Keys
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string QUALITY_LEVEL_KEY = "quality_level";
+    const string RESOLUTION_KEY = "resolution_choice";
+    const string FULLSCREEN_KEY = "fullscreen_choice";
+    #endregion
+
+    #region Methods 
+
+    #region Master Volume
+    public static void SetMasterVolume(float volume)
+    {
+        PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY, 8f);
+    }
+    #endregion
+
+    #region QualityLevel
+    public static void SetQualityLevel(int value)
+    {
+        PlayerPrefs.SetInt(QUALITY_LEVEL_KEY, value);
+    }
+
+    public static int GetQualityLevel()
+    {
+        return PlayerPrefs.GetInt(QUALITY_LEVEL_KEY, 5);
+    }
+    #endregion
+
+    #region Resolution
+    public static void SetResolution(int value)
+    {
+        PlayerPrefs.SetInt(RESOLUTION_KEY, value);
+    }
+
+    public static int GetResolution()
+    {
+        return PlayerPrefs.GetInt(RESOLUTION_KEY, Screen.resolutions.Length - 1);
+    }
+    #endregion
+
+    #region FullScreen
+    public static void SetFullScreen(bool value)
+    {
+        int intValue = value ? 1 : 0;
+        PlayerPrefs.SetInt(FULLSCREEN_KEY, intValue);
+    }
+
+    public static bool GetFullScreen()
+    {
+        return (PlayerPrefs.GetInt(FULLSCREEN_KEY, 1) == 1);
+    }
+    #endregion
+
+    #endregion
+
+    private void OnEnable()
+    {
+        Resolution startRes = Screen.resolutions[PlayerPrefsManager.GetResolution()];
+        Screen.SetResolution(startRes.width, startRes.height, GetFullScreen());
+    }
+}
---
+++ Assets/UI/Sounds/PersistentMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistentMusic : MonoBehaviour
+{
+    #region Varuabiles
+    public AudioClip backgroundLoop;
+
+    private AudioSource audioS;
+    #endregion
+
+    #region Initialization
+    private void Start()
+    {
+        StartMusic();
+        DontDestroyOnLoad(this);
+    }
+
+
+    private void StartMusic()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.clip = backgroundLoop;
+        audioS.volume = PlayerPrefsManager.GetMasterVolume();
+        audioS.Play();
+    }
+    #endregion
+}

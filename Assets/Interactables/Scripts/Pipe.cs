c9eb063ac7cdedcea55f0155361751152f7a71e3
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-11 10:52:48 +0200

    Change color pipe script

---
+++ Assets/Interactables/Scripts/Pipe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Update the color of the pipe according to the substance's color.
+ */
+
+public class Pipe : MonoBehaviour
+{
+
+	private void Start ()
+    {
+        Generator particleGenerator = GetComponentInChildren<Generator>();
+        SpriteRenderer rend = GetComponentInChildren<SpriteRenderer>();
+
+        Color substanceColor = particleGenerator.particleSubstance.particleColor;
+        rend.color = substanceColor;
+	}
+	
+}

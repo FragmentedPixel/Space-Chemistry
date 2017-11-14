3b0e6583f62dec88e8debc84130626db295bb124
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-14 22:37:56 +0200

    Teleporter Rudimentar

---
+++ Assets/Interactables/Scripts/Teleporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform destination;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position = destination.position;
+    }
+
+
+}

c20e74bb4e97301f1cf59b073056b67f675d8648
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-14 21:12:03 +0200

    Particle Destoryer

---
+++ Assets/ParticleDestoryer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestoryer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Particle particle = col.gameObject.GetComponent<Particle>();
+
+        if(particle != null)
+        {
+            ParticlePool.instance.ReturnParticle(particle);
+        }
+    }
+}

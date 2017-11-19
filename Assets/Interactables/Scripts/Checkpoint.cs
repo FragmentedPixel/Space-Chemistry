908b6418db3668be2e55a737f799d9eda09cc9a2
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-19 12:19:33 +0200

    Checkpoint Sound

--- Assets/Interactables/Scripts/Checkpoint.cs
+++ Assets/Interactables/Scripts/Checkpoint.cs
@@ -11,21 +11,35 @@ public class Checkpoint : MonoBehaviour
     // checked sprite.
     public Sprite checkpointPassed;
 
+    // Sound played when the player passes the checkpoint.
+    public AudioClip passedSound;
+
+    //Was this checkpoint already passed?
+    private bool passed = false;
+
     // Renderer component.
     private SpriteRenderer spriterenderer;
+
+    // AudioS component.
+    private AudioSource audioS;
 
     void Start()
     {
         spriterenderer = GetComponentInChildren<SpriteRenderer>();
+        audioS = GetComponent<AudioSource>();
+
+        audioS.volume = PlayerPrefsManager.GetMasterVolume();
     }
 	
     private void OnTriggerEnter2D(Collider2D other)
     {
         PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
 
-        if (player != null)
+        if (player != null && passed == false)
         {
+            passed = true;
             spriterenderer.sprite = checkpointPassed;
+            audioS.PlayOneShot(passedSound);
             player.SetCheckPoint(transform.position);
         }
     }

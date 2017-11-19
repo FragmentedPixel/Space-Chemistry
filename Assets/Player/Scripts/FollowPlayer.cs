94615af4ecc8345aa01db6c01b2f4e673be5d42d
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-19 21:37:22 +0200

    Mid boundary & Folder structures

---
+++ Assets/Player/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+    private Transform player;
+
+    private void Start()
+    {
+        PlayerMovement playerMov = FindObjectOfType<PlayerMovement>();
+        target = playerMov.cameraPoint;
+        player = playerMov.transform;
+    }
+
+    private void Update()
+    {
+        //TODO: Boundary when the player reaches the camera.
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, -10f), Time.deltaTime);
+       
+        // Pentru cand iti dai seama ca are stutterstep.
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, -10f), speed * Time.deltaTime);
+    }
+
+}

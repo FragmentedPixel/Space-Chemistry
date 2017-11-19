fe9dc1e8ebf068f57e51790d97fb015582ad2b19
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-19 23:38:40 +0200

    Basic Level prefab

---
+++ Assets/Player/Scripts/LevelStarter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStarter : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject cameraPrefab;
+    public GameObject UIprefab;
+    public GameObject particleManagerPrefab;
+    public GameObject levelManangerPrefab;
+
+    private void Start()
+    {
+        Instantiate(playerPrefab, transform.position, transform.rotation, transform);
+        Instantiate(cameraPrefab, transform.position, transform.rotation, transform);
+        Instantiate(UIprefab, transform);
+        Instantiate(particleManagerPrefab, transform);
+        Instantiate(levelManangerPrefab, transform);
+    }
+
+
+}

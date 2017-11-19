8d95b23137d24eaaf93545f29d07ddc9cad0b4ec
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-19 20:57:11 +0200

    Second Method

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public bool mirceaChoice = false;
+    public bool mirceaChoice = true;
     public float speed;
     private Transform target;
 

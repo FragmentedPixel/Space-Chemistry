ebf26f9856b3e4d20eba5191e8897570374163e2
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-20 10:16:14 +0200

    Camera zoom trigger

---
+++ Assets/Interactables/Scripts/CameraZoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float zoom = 5;
+    private float initzoom;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+
+        if(player != null)
+        {
+            FollowPlayer[] cameras = FindObjectsOfType<FollowPlayer>();
+            initzoom = cameras[0].GetComponent<Camera>().orthographicSize;
+            foreach(FollowPlayer camera in cameras)
+            {
+                camera.GetComponent<Camera>().orthographicSize = zoom;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+
+        if (player != null)
+        {
+            FollowPlayer[] cameras = FindObjectsOfType<FollowPlayer>();
+            foreach (FollowPlayer camera in cameras)
+            {
+                camera.GetComponent<Camera>().orthographicSize = initzoom;
+            }
+        }
+    }
+
+}

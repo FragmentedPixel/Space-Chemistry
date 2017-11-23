731b7f46c453ea1468680bd06062168c5a5b9b36
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-23 23:45:12 +0200

    Substance trigger octimization

---
+++ Assets/Substances/Scripts/SubstancesOptimizator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubstancesOptimizator : MonoBehaviour
+{
+    private void Awake()
+    {
+        Generator[] generators = FindObjectsOfType<Generator>();
+        foreach(Generator gen in generators)
+        {
+            gen.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Generator generator = collision.gameObject.GetComponent<Generator>();
+        if(generator != null)
+        {
+            generator.enabled = true;
+        }
+    }
+}

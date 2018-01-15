10af654e5fb147b67fc342fbf138c86a556dae0b
Oprea Alexandru <oalexprea@yahoo.com>
2018-01-15 22:59:42 +0200

    Smooth Camera & Hand Rotation

---
+++ Assets/Player/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+
+	public Transform target;
+
+	public float smoothSpeed = 12.5f;
+	public Vector3 offset;
+
+	void FixedUpdate()
+	{
+		if (target == null) 
+		{
+			target = FindObjectOfType<PlayerHealth> ().transform;
+			offset = transform.position - target.position;
+		}
+
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.fixedDeltaTime);
+
+		transform.position = smoothedPosition;
+	}
+
+}

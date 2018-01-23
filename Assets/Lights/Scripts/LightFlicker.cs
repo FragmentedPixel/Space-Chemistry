dd2635f164f93fe6f738d7ec2b8415f894e81d2b
Oprea Alexandru <oalexprea@yahoo.com>
2018-01-23 22:03:22 +0200

    The flicker written & working

--- Assets/Lights/Scripts/LightFlicker.cs
+++ Assets/Lights/Scripts/LightFlicker.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class LightFlicker : MonoBehaviour {
 
-    public Light thelight; 
+    public Light light;
+
+    public float speed;
+    public float noise;
+
 	// Use this for initialization
 	void Start ()
     {
-        thelight = GetComponent<Light>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        light = GetComponent<Light>();
+        StartCoroutine(Flicker());
+
+    }
+
+
+    IEnumerator Flicker()
     {
-         
-	}
+        light.enabled = true;
+        float randNoise = Random.Range(-1, 1) * Random.Range(-noise, noise);
+        yield return new WaitForSeconds(speed + randNoise);
+        light.enabled = false;
+        yield return new WaitForSeconds(speed);
+        StartCoroutine(Flicker());
+    }
 }

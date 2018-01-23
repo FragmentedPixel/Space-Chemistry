e896cecfc6f1b8fa83d4763849a0c4f94dfc0019
Oprea Alexandru <oalexprea@yahoo.com>
2018-01-23 22:42:38 +0200

    Level 1 & Shake Trigger

---
+++ Assets/Explosion/Scripts/ShakeTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTrigger : OneTimeEvent {
+
+    public float shakeDuration;
+    public AudioClip impactSound;
+
+    private AudioSource audioS;
+
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.volume = PlayerPrefsManager.GetMasterVolume();
+    }
+
+    public override void ActionHappening()
+    {
+        audioS.PlayOneShot(impactSound);
+        FindObjectOfType<CameraShake>().Shake(shakeDuration);
+    }
+}

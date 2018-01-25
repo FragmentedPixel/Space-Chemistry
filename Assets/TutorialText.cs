26b39ee6464b001ad702fc74876dc1713e55dc2a
Oprea Alexandru <oalexprea@yahoo.com>
2018-01-25 22:00:22 +0200

    Dumb push

---
+++ Assets/TutorialText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour
+{
+    public bool controllerConnected;
+
+    [TextArea] public string KeyboardText;
+    [TextArea] public string GamepadText;
+
+    private void OnEnable()
+    {
+        if (controllerConnected)
+            GetComponent<Text>().text = KeyboardText;
+        else
+            GetComponent<Text>().text = GamepadText;
+    }
+
+}

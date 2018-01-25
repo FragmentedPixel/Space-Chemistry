9133530e698584d36c121e7198b0c3a996d0e8bd
Oprea Alexandru <oalexprea@yahoo.com>
2018-01-25 23:31:54 +0200

    Level 2 Repiar system

---
+++ Assets/StartConnecting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartConnecting : MonoBehaviour
+{
+    public float speed;
+    public Image[] containers;
+
+    private void Start()
+    {
+        StartFilling();
+    }
+
+    public void StartFilling()
+    {
+        StartCoroutine(FillingCR());
+    }
+
+    private IEnumerator FillingCR()
+    {
+        for(int i = 0; i < containers.Length; i++)
+        {
+            while(containers[i].fillAmount < 1)
+            {
+                containers[i].fillAmount += Time.deltaTime * speed;
+                yield return null;
+            }
+        }
+
+        FinishedFilling();
+    }
+
+    private void FinishedFilling()
+    {
+
+    }
+	
+}

68280b8c458d08f3d6d7e12756a000f80948d367
VladDuncea <vlad_duncea_31@yahoo.com>
2018-02-13 20:48:41 +0200

    SkillsUpdate

---
+++ Assets/UI -Menu/Scripts/SkillsManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillsManager : MonoBehaviour {
+
+    #region Variables
+
+    int freesp = 10;
+    int[] Skills=new int[3];
+    [HideInInspector]
+    public bool SkillsActive;
+
+    #endregion
+
+    #region Initialization
+    private void Start()
+    {
+        //read from serialization
+        for (int i = 0; i < Skills.Length; i++)
+            Skills[i] = Random.Range(0, 3);
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void Update()
+    {
+        //read from serialization
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            if(!SkillsActive)
+            {
+                
+            }
+            else
+            {
+
+            }
+            SkillsActive = !SkillsActive;
+            gameObject.SetActive(SkillsActive);
+        }
+    }
+
+    //Level up a certain skill
+    public void LevelUpSkill(int SkillID)
+    {
+        if (1 > freesp)
+            MessageManager.getInstance().DissplayMessage("Insufficient Funds(Like in the bank)", 3);
+        else
+        {
+            Skills[SkillID]++;
+            MessageManager.getInstance().DissplayMessage("Skill succesfully upgraded", 3);
+        }
+        
+    }
+    //Give the player a certain amount of skill points
+    public void GainSkillPoint(int value)
+    {
+        freesp += value;
+    }
+    //Return the number of free skill points
+    public int AvailableSkillPoints()
+    {
+        return freesp;
+    }
+
+    #endregion
+
+}

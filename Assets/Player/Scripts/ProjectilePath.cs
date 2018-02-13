4841290dc0c1d89ade24782623d00360d7da089e
Oprea Alexandru <oalexprea@yahoo.com>
2018-02-13 18:43:31 +0200

    Projectiles arrows

--- Assets/Player/Scripts/ProjectilePath.cs
+++ Assets/Player/Scripts/ProjectilePath.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ProjectilePath : MonoBehaviour
 {
     public float power = 5f;
-    public Material projectileMaterial;
+    public GameObject arrowPrefab;
 
     float force = 4.0f;
     int samples = 25;
@@ -26,13 +26,8 @@ public class ProjectilePath : MonoBehaviour
         argo = new GameObject[samples];
         for (var i = 0; i < argo.Length; i++)
         {
-            var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            go.GetComponent<Collider>().enabled = false;
-            go.GetComponent<Renderer>().material = projectileMaterial;
-            go.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            go.transform.parent = transform;
+            var go = Instantiate(arrowPrefab, transform.position, transform.rotation, transform);
             argo[i] = go;
-
         }
     }
 	
@@ -47,7 +42,7 @@ public class ProjectilePath : MonoBehaviour
     {
         for (var i = 0; i < argo.Length; i++)
         {
-            argo[i].GetComponent<MeshRenderer>().enabled = show;
+            argo[i].GetComponent<SpriteRenderer>().enabled = show;
             argo[i].transform.position = home;
         }
     }
@@ -59,6 +54,7 @@ public class ProjectilePath : MonoBehaviour
         if (!MyImputManager.connectedToController)
         {
             Vector3 handPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            handPosition.z = transform.position.z;
             indicator = (handPosition - transform.position).normalized * power;
         }
         else
@@ -66,10 +62,7 @@ public class ProjectilePath : MonoBehaviour
 			indicator = transform.right * power;
         }
 
-
-
-
-        argo[0].transform.position = transform.position;
+        argo[0].GetComponent<SpriteRenderer>().enabled = false;
         var v3 = transform.position;
         var y = (force * (indicator)).y;
         float t = 0.0f;
@@ -79,7 +72,13 @@ public class ProjectilePath : MonoBehaviour
             v3 += force * (indicator) * spacing;
             t += spacing;
             v3.y = y * t + 0.5f * Physics.gravity.y * t * t + transform.position.y;
+            v3.z = transform.parent.position.z;
             argo[i].transform.position = v3;
+        }
+
+        for(int i = 1; i < argo.Length-1; i++)
+        {
+            argo[i].transform.rotation = Quaternion.LookRotation(argo[i - 1].transform.position - argo[i].transform.position, transform.forward);
         }
     }
 }

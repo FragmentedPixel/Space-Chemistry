f20ad582f0f04383883db8e0f1405cde7c36506f
Oprea Alexandru <oalexprea@yahoo.com>
2018-03-07 22:01:59 +0200

    Fixed Invetory & Fog Prefab

--- Assets/Player/Scripts/InvetoryUI.cs
+++ Assets/Player/Scripts/InvetoryUI.cs
@@ -15,7 +15,8 @@ public class InvetoryUI : UIMenu
 
         // If there are no collectabiles in this level, we don't need the pannel.
         CollectableObject[] objectsToCollect = FindObjectsOfType<CollectableObject>();
-        return (objectsToCollect.Length <= 0);
+        
+        return (objectsToCollect.Length < 0);
     }
 
     public void HighLight()

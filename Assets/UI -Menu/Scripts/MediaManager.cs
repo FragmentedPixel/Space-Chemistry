a1e9217e751ab7146f99f966ae301d43e2077847
Oprea Alexandru <oalexprea@yahoo.com>
2018-03-20 17:20:51 +0200

    Media Links added to mai menu and thanks for playing

---
+++ Assets/UI -Menu/Scripts/MediaManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediaManager : MonoBehaviour
+{
+    private void OpenUrl(string urlToOpen)
+    {
+        Application.OpenURL(urlToOpen);
+    }
+
+    public void OpenFaceBook()
+    {
+        OpenUrl("https://www.facebook.com/DevSpaceChemistry/");
+    }
+
+    public void OpenInstagram()
+    {
+        OpenUrl("https://www.instagram.com/spacechemistry/");
+    }
+
+    public void OpenTwitter()
+    {
+        OpenUrl("https://twitter.com/space_chemistry");
+    }
+
+    public void OpenWebsite()
+    {
+        OpenUrl("http://spacechemistry.fragmentedpixel.com/");
+    }
+
+}

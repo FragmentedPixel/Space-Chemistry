8089c85a7c8e1a1474b95aa1f747ddbe9f0cee47
Oprea Alexandru <oalexprea@yahoo.com>
2018-03-25 12:18:16 +0300

    Designer Variabiles & new pipes

---
+++ Assets/Designer Code/Variables/FloatVariable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class FloatVariable : ScriptableObject
+{
+    [Tooltip("Description of the variable.")]
+    [TextArea] [SerializeField] private string description;
+
+    [SerializeField] private float defaultValue;
+
+    private float currentValue;
+
+    public float value
+    {
+        get { return currentValue; }
+        set {
+            OnVariableChanged();
+            currentValue = value;
+        }
+    }
+
+    private void OnEnable()
+    {
+        currentValue = defaultValue;
+    }
+
+    private void OnVariableChanged()
+    {
+        Debug.Log("Variable was changed.");
+    }
+
+
+}

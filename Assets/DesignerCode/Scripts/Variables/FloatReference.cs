46611549a879e156891129dc6374427fc9cd282b
Oprea Alexandru <oalexprea@yahoo.com>
2018-04-01 23:19:37 +0300

    Designer Code sub Module added

---
+++ Assets/DesignerCode/Scripts/Variables/FloatReference.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class FloatReference : GenericReference<float, FloatVariable>
+{
+    public FloatReference(float value) : base(value)
+    {
+
+    }
+}
---
+++ Assets/DesignerCode/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+
+[Serializable]
+[CreateAssetMenu(fileName = "Float Variable", menuName = "Variables/Float", order = 0)]
+public class FloatVariable : GenericVariable<float>
+{
+
+}
+
+[Serializable]
+public class FloatEventToRaiseAtValue : EventToRaiseAtValue<float>
+{
+
+}
